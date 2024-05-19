5ce2d33c0f4e38077e7829a05adb5e15a017fd25
Raftouch <rafanelkin@gmail.com>
2024-05-19 17:38:17 +0200

    add todo tiles, change color, add checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/components/todo-tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,15 +12,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amber[200],
+        backgroundColor: Colors.green[200],
         appBar: AppBar(
-          backgroundColor: Colors.amber,
+          backgroundColor: Colors.green,
           title: Text('To Do'),
           elevation: 0,
         ),
         body: ListView(
           children: [
-            ToDoList(),
+            TodoTile(),
+            TodoTile(),
+            TodoTile(),
+            TodoTile(),
           ],
         ));
   }
