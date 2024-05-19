f17869ee8ae4f83bd180529804d9cf4a213c9ed5
Raftouch <rafanelkin@gmail.com>
2024-05-19 19:01:17 +0200

    add buttons

--- lib/components/dialog_box.dart
+++ lib/components/dialog_box.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/components/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const AlertDialog(
+    return AlertDialog(
       backgroundColor: Colors.green,
       content: SizedBox(
         height: 120,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                   border: OutlineInputBorder(), hintText: "Add a new task"),
             ),
-             Row(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 // save button
+                MyButton(text: "Save", onPressed: () {}),
 
                 // cancel button
+                MyButton(text: "Cancel", onPressed: () {}),
               ],
             )
           ],
