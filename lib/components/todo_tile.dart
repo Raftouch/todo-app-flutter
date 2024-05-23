36ecb596c0d4fc912d94a46d138257ec030c9246
Raftouch <rafanelkin@gmail.com>
2024-05-23 16:24:20 +0200

    add slidable widget

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskName;
@@ -16,26 +17,34 @@ class TodoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 24),
-      child: Container(
-        padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-            color: Colors.green, borderRadius: BorderRadius.circular(12)),
-        child: Row(
-          children: [
-            // value --> true or false // onChanged --> switch between true or false
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                  decoration: taskCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
-            ),
-          ],
+      child: Slidable(
+        endActionPane: const ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+              onPressed: deleteTask,
+              icon: Icons.delete,
+              backgroundColor: Colors.red)
+        ]),
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+              color: Colors.green, borderRadius: BorderRadius.circular(12)),
+          child: Row(
+            children: [
+              // value --> true or false // onChanged --> switch between true or false
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
         ),
       ),
     );
